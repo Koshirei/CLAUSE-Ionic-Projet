54cc5be05d1814e3044d24e2ff25d37a1af0853e
Koshirei <axelaxelc@aol.fr>
2023-03-21 18:54:48 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CLAUSE-Ionic-Projet',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
